691e1579cac0f8807eeb5b81fd0d95a4d2dce350
Jayesh Vashishtha <31667655+jayeshsharma01@users.noreply.github.com>
2021-10-30 21:15:23 +0530

    added 2 integer addition

--- code.c
+++ code.c
@@ -1,9 +1,17 @@
-
 #include <stdio.h>
+int sum (int a , int b)
+{ int c;
+ c=a+b;
+ return c;
+}
 
 int main() {
+    int a,b,c;
+    printf("please enter 2 integers\n");
+    scanf("%d %d",&a,&b);
+    c= sum(a,b);
+    printf("sum of %d and %d is %d",a,b,c);
     
-    printf("Hello world");
     
     return 0;
 }
